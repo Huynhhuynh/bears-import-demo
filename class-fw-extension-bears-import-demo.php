6ac3f7575153e6c7723fc7b5ac892702863f3a88
Hieu Huynh <conghieuhuynh91@gmail.com>
2018-11-12 15:40:54 +0700

    update

--- class-fw-extension-bears-import-demo.php
+++ class-fw-extension-bears-import-demo.php
@@ -8,5 +8,26 @@ class FW_Extension_Bears_Import_Demo extends FW_Extension {
      */
     protected function _init() {
         // ...
+        $this->hooks();
+    }
+
+    public function hooks() {
+        add_action( 'admin_menu', array( $this, 'import_demo_register_ref_page' ) );
+    }
+
+    public function import_demo_register_ref_page() {
+        add_submenu_page(
+            'tools.php',
+            __( 'Import Demo', 'educlever' ),
+            __( 'Import Demo', 'educlever' ),
+            'manage_options',
+            'import-demo-page',
+            array( $this, 'import_demo_page_callback' ) );
+    }
+
+    public function import_demo_page_callback() {
+        ?>
+        Hello
+        <?php
     }
 }
