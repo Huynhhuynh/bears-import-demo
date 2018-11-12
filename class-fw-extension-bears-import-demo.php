fece86dfc5d8f6172f9cfce831abcccbb35560d2
Hieu Huynh <conghieuhuynh91@gmail.com>
2018-11-12 15:12:57 +0700

    first commit

---
+++ class-fw-extension-bears-import-demo.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+class FW_Extension_Bears_Import_Demo extends FW_Extension {
+	/**
+     * @internal
+     */
+    protected function _init() {
+        // ...
+    }
+}
