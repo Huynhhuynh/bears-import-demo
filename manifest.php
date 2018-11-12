38e221a5b94da462dfec9731ff2a49d69c04bc6a
Hieu Huynh <conghieuhuynh91@gmail.com>
2018-11-12 16:47:55 +0700

    update manifest.php

--- manifest.php
+++ manifest.php
@@ -2,6 +2,10 @@
 
 $manifest = array();
 
-// $manifest['standeduclever'] = true;
-$manifest['version']    = '1.0.1';
-$manifest['github_repo'] = 'https://github.com/Huynhhuynh/bears-import-demo';
+$manifest['name']               = __('Bears Import Demo', 'educlever');
+$manifest['author']             = 'Bearsthemes';
+$manifest['author_uri']         = 'https://bearsthemespremium.com/';
+$manifest['standalone']         = true;
+$manifest['display']            = true;
+$manifest['version']            = '1.0.1';
+$manifest['github_repo']        = 'https://github.com/Huynhhuynh/bears-import-demo';
